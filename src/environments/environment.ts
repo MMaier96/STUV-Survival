4656f3e3afb8090279437004ab82209e412bca0b
Robert Zebec <zebec.robert@gmail.com>
2019-03-25 23:23:23 +0100

    Fixed time offset and changed default api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  backendUrl: 'http://marcelmaier.ddns.net:3000/api/'
+  backendUrl: 'https://survivalapi.herokuapp.com/survivalapi/'
 };
 
 /*
